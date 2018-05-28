f06bd092ef67059dc97ae197fdbad2f60606cb49
RobRetro <robetyrob18@gmail.com>
2018-05-28 18:38:08 +0100

    new file:   media/menu.sh 	new file:   media/mpg123.sh 	new file:   menu.sh 	deleted:    test.txt

---
+++ media/menu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# rpi toolkit
+# rob retro
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Raspberry Pi Toolkit"
+TITLE="select something"
+MENU="Choose one of the following options:"
+
+OPTIONS=(1 "Install MPG123 (cli audio player)"
+         2 "2"
+         3 "3")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            sudo bash mpg123.sh
+            ;;
+        2)
+            echo "You chose Option 2"
+            ;;
+        3)
+            echo "You chose Option 3"
+            ;;
+esac
